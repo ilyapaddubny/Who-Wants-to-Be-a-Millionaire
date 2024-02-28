66006230ecbd56951758f7446aca48447bc454ba
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2024-02-28 20:34:32 +0100

    add autolayout

--- Who Wants to Be a Millionaire/SceneDelegate.swift
+++ Who Wants to Be a Millionaire/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let artistVC = StartGameViewController()
-        let navigationController = UINavigationController(rootViewController: ProgressViewController())
+        let navigationController = UINavigationController(rootViewController: artistVC)
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
         
