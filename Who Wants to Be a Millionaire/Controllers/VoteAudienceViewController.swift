325c26d6929f95d81e628896d09bc211310825c1
vsiisv <shores.freezer0u@icloud.com>
2024-03-02 21:23:25 +1000

    VotingGraphView

---
+++ Who Wants to Be a Millionaire/Controllers/VoteAudienceViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VoteAudienceViewController.swift
+//  Who Wants to Be a Millionaire
+//
+//  Created by VASILY IKONNIKOV on 02.03.2024.
+//
+
+import UIKit
+
+class VoteAudienceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
