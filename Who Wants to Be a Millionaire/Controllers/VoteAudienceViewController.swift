cfadc91d855418ffd8f6731d6108c2a417d2009c
vsiisv <shores.freezer0u@icloud.com>
2024-03-02 21:34:26 +1000

    Added logic for percentage generation

--- Who Wants to Be a Millionaire/Controllers/VoteAudienceViewController.swift
+++ Who Wants to Be a Millionaire/Controllers/VoteAudienceViewController.swift
@@ -21,8 +21,8 @@ class VoteAudienceViewController: UIViewController {
 	weak var delegate: VoteAudienceViewControllerDelegate?
 	
 	private var votePercentages: [Int] = [0, 0, 0, 0]
-	private var correctAnswerIndex: Int?
-	private var questionDifficulty: QuestionDifficulty?
+	private var correctAnswerIndex: Int? = 1
+	private var questionDifficulty: QuestionDifficulty? = .easy
 	private var votingGraphView = VotingGraphView()
 	
 	private lazy var backgroundImage: UIImageView = {
@@ -36,12 +36,89 @@ class VoteAudienceViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 		
+		setupCorrectAnswer()
+		setupQuestionDifficulty()
+		votingGraphView.delegate = self
+		
+		generateVoteData(with: correctAnswerIndex)
 		addSubviews()
 		setupVotingGraphView()
 		setupConstraints()
     }
 
+	private func setupCorrectAnswer() {
+		if let correctAnswer = delegate?.getCorrectAnswer(self) {
+			self.correctAnswerIndex = correctAnswer
+		}
+	}
+	
+	private func setupQuestionDifficulty() {
+		if let questionDifficulty = delegate?.getQuestionDifficulty(self) {
+			self.questionDifficulty = questionDifficulty
+		}
+	}
 
+}
+
+// MARK: - Vote Data Generation
+extension VoteAudienceViewController {
+	// Выбор с какой вероятностью будет произведен выбор аудитории
+	private func determineCorrectChoiceProbability() -> Int {
+		switch questionDifficulty {
+		case .easy:
+			return 70
+		case .hard:
+			return 50
+		case .none:
+			return 50
+		}
+	}
+	
+	// Определяет, выберет ли аудитория правильный ответ
+	private func willAudienceChooseCorrectly(with probability: Int) -> Bool {
+		let totalProbability = 100
+		let probabilitySample = Int.random(in: 1...totalProbability)
+		return probabilitySample < probability
+	}
+
+	// Распределяет оставшиеся проценты для неправильных ответов.
+	private func generateOtherPercentage(firstRandom: Int) -> [Int] {
+		var remainingPercentage = 100 - firstRandom
+		var percentages: [Int] = []
+		for _ in 0..<2 {
+			let randomPercentage = Int.random(in: 0...remainingPercentage)
+			percentages.append(randomPercentage)
+			remainingPercentage -= randomPercentage
+		}
+		percentages.append(remainingPercentage)
+		return percentages
+	}
+
+	private func generateVoteData(with correctAnswerIndex: Int?) {
+		let correctChoiceProbability = determineCorrectChoiceProbability()
+		let willAudienceChooseCorrectly = willAudienceChooseCorrectly(with: correctChoiceProbability)
+
+		let correctAnswerPercentage = willAudienceChooseCorrectly ? Int.random(in: 51...100) : Int.random(in: 0...50)
+		let otherAnswersPercentage = generateOtherPercentage(firstRandom: correctAnswerPercentage)
+		
+		// Установка процентов голосов
+		var index = 0
+		for i in 0..<4 {
+			if i == correctAnswerIndex {
+				votePercentages[i] = correctAnswerPercentage
+			} else {
+				votePercentages[i] = otherAnswersPercentage[index]
+				index += 1
+			}
+		}
+	}
+}
+
+// MARK: - VotingGraphViewDelegate
+extension VoteAudienceViewController: VotingGraphViewDelegate {
+	func votingGraphViewNeedsVotePercentages(_ votingGraphView: VotingGraphView) -> [Int] {
+		return votePercentages
+	}
 }
 
 // MARK: - Style and Constraints
